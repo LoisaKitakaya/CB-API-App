e29530c7b725def9330a45dc83420612dbefb106
Freedom Loisa <kitakayaloisa@gmail.com>
2022-06-17 13:18:17 +0300

    changed name displayed in admin panel

--- cat_species/urls.py
+++ cat_species/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cats.urls')),
 ]
+
+admin.site.site_header = 'Cat Breeds API App Admin Panel'
+admin.site.site_title = 'Cat Breeds API App Backend'
